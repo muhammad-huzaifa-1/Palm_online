891dc2634b2bcabbe94664c28445992e375932aa
muhammad-huzaifa-1 <huzaifaktk0905@gmail.com>
2024-11-21 18:51:30 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
             method:"post",
             body: JSON.stringify({email,password}),
             headers:{
-                "Content-Type" : "application/json"
+                "Content-Type" : "application/json",
+                'Access-Control-Allow-Origin': 'true'
             }
         });
 
