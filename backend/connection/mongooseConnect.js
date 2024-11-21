e87fc456f1a9a7998424314cc682108c6e428f5e
muhammad-huzaifa-1 <huzaifaktk0905@gmail.com>
2024-11-21 21:21:14 +0500

    Update mongooseConnect.js

--- backend/connection/mongooseConnect.js
+++ backend/connection/mongooseConnect.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const Connection = mongoose.connect(process.env.BACKEND_MONGOOSE_CONNECTION);
-// const Connection = mongoose.connect("mongodb://localhost:27017/Data")
-
+let Connection = mongoose.connect("mongodb+srv://huzaifaktk0905:G0khuZAifAEr@mongocluster.ld6y3.mongodb.net/Data");
 module.exports = Connection;
